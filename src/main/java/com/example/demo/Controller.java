4a6cf36f41b186c581a86fb0fa0efc0d5b34745a
tausifahmad38 <tausif.ahmad@hcl.com>
2023-11-02 13:07:37 +0530

    first commit

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,27 +1,13 @@
-//package com.example.demo;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class Controller {
-//
-//	@Autowired
-//	private MailService emailService;
-//
-//	@GetMapping("/ch")
-//	public String chk() {
-//		return "HI";
-//	}
-//	
-//	
-//	@GetMapping("/sendEmail")
-//	public String sendEmail() {
-//		String to = "ahmadtausif38@gmail.com";
-//		String subject = "Hello from Spring Boot";
-//		String text = "<h1>Hello there!</h1><p>This is a sample email sent from a Spring Boot application.</p>";
-//		emailService.sendEmail(to, subject, text);
-//		return "Email sent successfully!";
-//	}
-//}
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	@GetMapping("/check")
+	public String show(){
+		return "<b>Hi..!<b><br> Its working fine....";
+	}
+
+}
